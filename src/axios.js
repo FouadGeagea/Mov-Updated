939b5997d79f3e8dfc874e5db0128f0cbe1ae1b4
FouadGeagea <72972431+FouadGeagea@users.noreply.github.com>
2021-01-18 17:30:55 +0200

    Add files via upload

---
+++ src/axios.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    // eslint-disable-next-line no-undef
+    baseURL:`https://api.themoviedb.org/3/discover${process.env.REACT_APP_GET}`
+});
+export default instance;
